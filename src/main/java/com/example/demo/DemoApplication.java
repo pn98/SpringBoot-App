7ee52198a9464a35e7adae8bd1f5f971ec1aa275
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-09-27 19:50:47 +0100

    Thymeleaf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,23 +3,24 @@ package com.example.demo;
 import com.example.demo.student.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/Member")
-	public List<Member> runner() {
-		return List.of(
+	@GetMapping("/members")
+	public String getMembers(Model model) {
+		List<Member> members = List.of(
 				new Member(
 						"Peter Nicholl",
 						22,
@@ -30,7 +31,21 @@ public class DemoApplication {
 						"12345",
 						true,
 						LocalDate.of(2023, 9, 20)
+				),
+				new Member(
+						"John Doe",
+						30,
+						2,
+						"Male",
+						"456 Fit Ave, Muscle Town",
+						"987-654-3210",
+						"54321",
+						false,
+						null
 				)
 		);
+
+		model.addAttribute("members", members);
+		return "members";  // Returns the members.html template
 	}
 }
